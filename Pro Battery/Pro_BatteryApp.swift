89ea957d980294a026eceda823b2ea640e5aee4d
TopScrech <sergei_saliukov@icloud.com>
2024-10-08 20:33:21 +0200

    Initial Commit

---
+++ Pro Battery/Pro_BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_BatteryApp.swift
+//  Pro Battery
+//
+//  Created by Sergei Saliukov on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Pro_BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
