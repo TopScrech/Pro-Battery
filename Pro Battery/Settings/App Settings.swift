eca2760f9ccbf0b9f1f7889a51595ea06228ad8c
TopScrech <sergei_saliukov@icloud.com>
2025-05-21 10:03:03 +0200

    improved settings

---
+++ Pro Battery/Settings/App Settings.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import LaunchAtLogin
+
+struct AppSettings: View {
+    var body: some View {
+        Form {
+            LaunchAtLogin.Toggle()
+        }
+        .navigationTitle("Settings")
+        .buttonStyle(.plain)
+        .formStyle(.grouped)
+        .frame(width: 500, height: 600)
+    }
+}
+
+#Preview {
+    AppSettings()
+}
