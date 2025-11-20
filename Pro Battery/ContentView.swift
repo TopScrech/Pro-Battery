34ca21f7feb1e04801621061bdaf9faa6e8b5f88
TopScrech <sergei_saliukov@icloud.com>
2025-11-20 09:54:52 +0100

    replaed ListParam with LabeledContent

--- Pro Battery/ContentView.swift
+++ Pro Battery/ContentView.swift
@@ -7,26 +7,26 @@ struct ContentView: View {
     var body: some View {
         List {
             Section {
-                ListParam("Condition", param: vm.condition)
-                ListParam("Cycle Count", param: vm.cycleCount.description)
-                ListParam("Design cycle count", param: vm.designCycleCount.description)
-                ListParam("Maximum Capacity", param: vm.maximumCapacity.description)
-                ListParam("Low Power Mode", param: vm.isLowPowerMode ? "Yes" : "No")
-                ListParam("Battery Level", param: vm.stateOfCharge.description)
-                ListParam("Temperature", param: "\(vm.temperature) °C")
+                LabeledContent("Condition", value: vm.condition)
+                LabeledContent("Cycle Count", value: vm.cycleCount.description)
+                LabeledContent("Design cycle count", value: vm.designCycleCount.description)
+                LabeledContent("Maximum Capacity", value: vm.maximumCapacity.description)
+                LabeledContent("Low Power Mode", value: vm.isLowPowerMode ? "Yes" : "No")
+                LabeledContent("Battery Level", value: vm.stateOfCharge.description)
+                LabeledContent("Temperature", value: "\(vm.temperature) °C")
             }
 #if DEBUG
             let chargingOrDischarging: LocalizedStringKey = vm.amperage >= 0 ? "Charging with" : "Discharging with"
-            ListParam(chargingOrDischarging, param: "\(vm.amperage) Watts")
+            LabeledContent(chargingOrDischarging, value: "\(vm.amperage) Watts")
             
-            ListParam("Time Remaining", param: vm.timeRemaining.description)
+            LabeledContent("Time Remaining", value: vm.timeRemaining.description)
             
-            ListParam("Charging", param: vm.isCharging ? "Yes" : "No")
+            LabeledContent("Charging", value: vm.isCharging ? "Yes" : "No")
             
 #warning("Have to be checked")
-            ListParam("Below warning level", param: vm.isBelowWarningLevel ? "Yes" : "No")
-            ListParam("Fully charged", param: vm.isFullyCharged ? "Yes" : "No")
-            // ListParam("High Power Mode", param: vm.isHighPowerMode ? "Yes" : "No")
+            LabeledContent("Below warning level", value: vm.isBelowWarningLevel ? "Yes" : "No")
+            LabeledContent("Fully charged", value: vm.isFullyCharged ? "Yes" : "No")
+            // LabeledContent("High Power Mode", value: vm.isHighPowerMode ? "Yes" : "No")
 #endif
             Section {
                 HStack {
